577afa3e489892fca7dd539480ace176900e5d1b
reksarka <reksarka@gmail.com>
2022-02-19 10:18:15 +0200

    Shell test runner is working properly.

--- utils/wp/test.sh
+++ utils/wp/test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-CORMORANT_TESTS_DIR=$TESTS_SUITE_DIR/tests
-
-$TESTS_SUITE_DIR/composer/vendor/phpunit/phpunit/phpunit \
-  --configuration $CORMORANT_TESTS_DIR/phpunit.xml.dist \
-  $CORMORANT_TESTS_DIR
+cd $TESTS_SUITE_DIR/tests
+$TESTS_SUITE_DIR/composer/vendor/phpunit/phpunit/phpunit
