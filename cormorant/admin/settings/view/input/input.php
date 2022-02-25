dfae329f90174763fbd2070bf7325544499ad0f4
reksarka <reksarka@gmail.com>
2022-02-25 23:32:44 +0200

    Improves setting name extraction for a view.

--- cormorant/admin/settings/view/input/input.php
+++ cormorant/admin/settings/view/input/input.php
@@ -5,6 +5,14 @@ require_once 'page_select.php';
 require_once 'text.php';
 require_once 'textarea.php';
 
+function input($input_type, $view_function, $options=[])
+{
+    $field_name = name($view_function);
+    $field_value = value($field_name);
+    $template = "\\view\\input\\$input_type";
+    $template($field_name, $field_value, $options);
+}
+
 function value($field_name)
 {
     $settings = get_option(\settings\NAME);
@@ -12,10 +20,12 @@ function value($field_name)
     return \settings\sanitize($field_name, $field_value);
 }
 
-function input($input_type, $view_function, $options=[])
+/**
+ * @return 'view\\name' -> 'name'
+ */
+function name($namespaced)
 {
-    $field_name = basename($view_function);
-    $field_value = value($field_name);
-    $template = "\\view\\input\\$input_type";
-    $template($field_name, $field_value, $options);
+    // Does it exists an oneliner in PHP?
+    $names = explode('\\', $namespaced);
+    return end($names);
 }
