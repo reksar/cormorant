fd97f94ff73ed5502cc950b4185924e8fcf1f361
reksarka <reksarka@gmail.com>
2022-03-27 13:34:43 +0300

    Switches the admin notify email template display.
    Respectively to the related checkbox checked.

--- cormorant/admin/settings/view/input/js/checkbox.js
+++ cormorant/admin/settings/view/input/js/checkbox.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
   const switchValue = checkbox => checkbox.value = checkbox.checked;
   document.querySelectorAll('input[type=checkbox]').forEach(checkbox =>
     checkbox.addEventListener('change', event =>
