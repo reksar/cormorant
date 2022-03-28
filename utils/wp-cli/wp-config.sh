1ebc04ef1841cec85569dd5674553cc2526de43f
reksarka <reksarka@gmail.com>
2022-03-28 15:20:09 +0300

    UPD: .env vars using.

--- utils/wp-cli/wp-config.sh
+++ utils/wp-cli/wp-config.sh
@@ -8,13 +8,12 @@ if ! wp core is-installed
 then
   echo Installing Wordpress...
 
-  # TODO: use .env
   wp core install \
-    --url="http://127.0.0.1:8000" \
-    --title="Cormorant test" \
-    --admin_user="admin" \
-    --admin_password="admin" \
-    --admin_email="admin@email.com"
+    --url="http://$WP_LOCAL_HOST:$WP_LOCAL_PORT" \
+    --title="Cormorant" \
+    --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASSWORD \
+    --admin_email=$WP_ADMIN_EMAIL
 
   wp config set WP_DEBUG true
   wp config set WP_DEBUG_LOG true
@@ -24,4 +23,4 @@ then
 fi
 
 echo
-echo Wordpress is ready on http://127.0.0.1:8000
+echo Wordpress is ready on http://$WP_LOCAL_HOST:$WP_LOCAL_PORT
